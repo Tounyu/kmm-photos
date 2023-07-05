34b10c89197a9c2090d6784a45a233c34df5069a
Tounyu <tounyu1992@gmail.com>
2023-07-06 06:21:07 +0900

    Add base kmm project

---
+++ shared/src/androidMain/kotlin/dev/tounyu/kmmphotos/Platform.android.kt
@@ -0,0 +1,7 @@
+package dev.tounyu.kmmphotos
+
+class AndroidPlatform : Platform {
+    override val name: String = "Android ${android.os.Build.VERSION.SDK_INT}"
+}
+
+actual fun getPlatform(): Platform = AndroidPlatform()
